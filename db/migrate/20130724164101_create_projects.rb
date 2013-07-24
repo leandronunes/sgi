9666c53812c9ada4ebdf5255c15fb1bc1db8b897
Leandro Nunes dos Santos <leandro.santos@serpro.gov.br>
2013-07-24 13:44:57 -0300

    adding project stuff

---
+++ db/migrate/20130724164101_create_projects.rb
@@ -0,0 +1,15 @@
+class CreateProjects < ActiveRecord::Migration
+  def self.up
+    create_table :projects do |t|
+      t.float :fp_predicted, :fp_realized
+      t.datetime :begin_date_predicted, :end_date_predicted, :begin_date_realized, :end_date_realized
+      t.string :code
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :projects
+  end
+end
